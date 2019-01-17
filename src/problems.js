aa6f1cb0a8ae6d445909e2919536593667082139
cesine <cesine@yahoo.com>
2019-01-17 15:39:09 -0500

    pairing

--- src/problems.js
+++ src/problems.js
@@ -3,12 +3,72 @@ Given an array of integers, return a new array of integers of the same size
 where each number in the array is replaced with the product of all numbers in the array except itself.
 
 For example: given the lists [1,2,3,4], the function should return [2*3*4, 1*3*4, 1*2*4, 1*2*3] -> [24, 12, 8, 6].
+[2]
 */
 
-function getProductsExceptItself() {
-  return 'hello world';
+// [24, 12, 4, 1] <- all products to the right
+// [1,1,2,6] <- all products to the left
+//
+//
+
+function getProductsExceptItselfMaxTime(input){
+  const out = []
+  const allRightProducts = [1]
+  const allLeftProducts = [1]
+  let product = 1
+  for(let i=input.length-2; i>=0; i--){
+    product *= input[i]
+    allRightProducts.push(product)
+  }
+  console.log('All Right: ', allRightProducts)
+
+  product = 1
+  for(let i=1; i<input.length; i++){
+    product*= input[i]
+    allLeftProducts.push(product)
+  }
+  console.log('All Left: ', allLeftProducts)
+
 }
 
+//
+function getProductsExceptItself(input) {
+  // iterate through the input
+  //  iterate through again
+  //    multiply by every value that isnt the current input
+  //
+  const out = []
+
+  for(let i=0; i<input.length; i++){
+    let product = 1
+    for(let j=0; j<input.length; j++){
+      if(j!==i){
+        product *= input[j]
+      }
+    }
+    out.push(product)
+  }
+
+  return out
+}
+
+function getProductsExceptItselfMap(input) {
+  return input.map((_, idx) => {
+    let product = 1
+
+    input.forEach((val, idx2) =>{
+      if(idx!== idx2){
+        product *= val
+      }
+    })
+
+    return product
+  })
+}
+
+
 module.exports = {
-  getProductsExceptItself: getProductsExceptItself
+  getProductsExceptItself: getProductsExceptItself,
+  getProductsExceptItselfMap: getProductsExceptItselfMap,
+  getProductsExceptItselfMaxTime: getProductsExceptItselfMaxTime
 };
--- test/someTests-specs.js
+++ test/someTests-specs.js
@@ -1,5 +1,5 @@
 const expect = require('expect.js');
-const { getProductsExceptItself } = require('../src/problems');
+const { getProductsExceptItselfMaxTime } = require('../src/problems');
 
 describe('Fun challenges', () => {
 
@@ -8,5 +8,25 @@ describe('Fun challenges', () => {
     expect(true).to.not.be(false);
     expect([1, 2, 3]).to.eql([1, 2, 3]);
   });
+
+  it('should return an empty array when an empty input is given', () => {
+    let input = []
+    let products = getProductsExceptItselfMaxTime(input)
+    expect(products).to.eql([])
+  })
+  it('should return [1] when given a single value array', () => {
+    let input = [2]
+    let products = getProductsExceptItselfMaxTime(input)
+    expect(products).to.eql([1])
+
+  })
+  it('should return an array of products when called multiple times', () =>{
+    let input = [1,2,3,4]
+    let input2 = [1,2,3,4]
+    let products = getProductsExceptItselfMaxTime(input)
+    expect(products).to.eql([24, 12, 8, 6])
+    expect(getProductsExceptItselfMaxTime(input2)).to.eql([24, 12, 8, 6])
+
+  })
 
 });
