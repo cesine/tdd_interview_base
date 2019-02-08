b68f85c077467bb510cd2c13439021f54ad26285
cesine <cesine@yahoo.com>
2019-02-08 14:48:44 -0500

    aum

--- src/problems.js
+++ src/problems.js
@@ -4,3 +4,26 @@ where each number in the array is replaced with the product of all numbers in th
 
 For example: given the lists [1,2,3,4], the function should return [2*3*4, 1*3*4, 1*2*4, 1*2*3] -> [24, 12, 8, 6].
 */
+// All numbers including decimals
+// [1, 2, 3]
+
+const productOfAll = (array) => {
+  const results = [];
+  // I will iterate through the array,
+  for(let i = 0 ; i < array.length; i++){
+    // We need rest of the elements to get the product
+    // create a variable to store product
+    // ignore i when i am multpling to the product
+    let product = 1;
+    for(let j = 0; j < array.length; j++){
+      if(i !== j) {
+         product *= array[j];
+      }
+    }
+    results.push(product);
+  }
+
+  return results;
+}
+
+module.exports =  productOfAll;
--- test/someTests-specs.js
+++ test/someTests-specs.js
@@ -1,11 +1,20 @@
 const expect = require('expect.js');
+const productOfAll = require('./../src/problems.js')
+
+// console.log(productOfAll);
 
 describe('Fun challenges', () => {
 
   it('should test expectations', () => {
-    expect(true).to.be(true);
-    expect(true).to.not.be(false);
-    expect([1, 2, 3]).to.eql([1, 2, 3]);
+    const res = productOfAll(1);
+    const res2 = productOfAll([1,2,3])
+    const res3 = productOfAll([2, 3, 4])
+    expect([]).to.eql([]);
+    expect(res2).to.eql([6, 3, 2]);
+    expect(res3).to.eql([12, 8, 6]);
+    expect(productOfAll([1])).to.eql([1]);
+    expect(productOfAll([2,2])).to.eql([2,2]);
+    expect(productOfAll([2,3,4,5])).to.eql([60,40,30,24]);
   });
 
 });
