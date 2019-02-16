bec8fa528c884b45412d70cae8493d9bbeb2adfd
cesine <cesine@yahoo.com>
2019-02-16 16:33:33 -0500

    scaffold testable

--- src/problems.js
+++ src/problems.js
@@ -4,3 +4,23 @@ where each number in the array is replaced with the product of all numbers in th
 
 For example: given the lists [1,2,3,4], the function should return [2*3*4, 1*3*4, 1*2*4, 1*2*3] -> [24, 12, 8, 6].
 */
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int yourMethod(int a, int b) {
+        System.out.println(a+b);
+        System.out.print(a*b);
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        assert yourMethod(1, 2) == 2 : "should equal 2";
+        System.out.print("It passes.");
+    }
+}
